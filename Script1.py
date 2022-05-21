dcf33f7fab0be9fa1c8fb1b6044eccf1e409e5db
Santy <santy@cierco.es>
2022-05-21 13:43:35 +0200

    primer commit

--- Script1.py
+++ Script1.py
@@ -39,6 +39,7 @@ print ((palabra+" ")*1000)
 
 #Realiza un programa que reciba una cantidad de minutos y muestre por pantalla a cuantas horas y minutos corresponde.
 #Por ejemplo: 1000 minutos son 16 horas y 40 minutos.
-
 minutos=int(input("Minutos:"))
 print ("Horas:%d - Minutos:%d" % (minutos/60,minutos%60))
+
+
