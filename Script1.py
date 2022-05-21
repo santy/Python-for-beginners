d681b145edb00a328b11d7957e09338ed0821958
Santy <santy@cierco.es>
2022-05-21 13:46:04 +0200

    1commit

--- Script1.py
+++ Script1.py
@@ -26,7 +26,6 @@ import math
 radio=float(input("Dime el radio:"))
 print ("Resultado: Area=%.2f Perimetro=%.2f" % (math.pi*radio**2,2*math.pi*radio))
 
-
 #Dados dos números, mostrar la suma, resta, división y multiplicación de ambos
 num1=float(input("Numero1:"))
 num2=float(input("Numero2:"))
@@ -35,7 +34,6 @@ print ("Suma:%d,resta:%d,multiplicacion:%d,division:%.2f"%(num1+num2,num1-num2,n
 # Escribir un programa que le pida una palabra al usuario, para luego imprimirla 1000 veces, con espacios intermedios.
 palabra=input("Dime una palabra:")
 print ((palabra+" ")*1000)
-
 
 #Realiza un programa que reciba una cantidad de minutos y muestre por pantalla a cuantas horas y minutos corresponde.
 #Por ejemplo: 1000 minutos son 16 horas y 40 minutos.
